37c0c6ef15ae4596423cb9acdd2ee11aed95ab01
Sergio Eduardo Flores Junior <sergio.flores.junior17@gmail.com>
2020-06-23 22:23:12 -0300

    add index page and reusability of navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,44 @@
-console.log('olá')
+$(document).ready(async () => {
+  iniciarPagina();
+  window.produtosPromocao = await carregaProdutos(produto => produto.isPromocao);
+  console.log('window.produtosPromocao', window.produtosPromocao);
+});
+
+function iniciarPagina() {
+  window.carregouNav = false;
+  // Carrega o componente de navbar
+  $.get('/src/components/navbar.html', data => {
+    $('#nav').html(data);
+    window.carregouNav = true;
+  });
+}
+
+function alterarNavbar(paginaAtual, tentativas = 0) {
+  // se atingiu o limite de tentativas, para a recursividade, deu problema na navbar.
+  if (tentativas > 5) return;
+
+  // se a navbar ainda não carregou (processo assíncrono) tenta novamente.
+  if (!window.carregouNav) return setTimeout(() => alterarNavbar(paginaAtual, tentativas + 1), 50);
+
+  // Remove as classes de ativo de todos os links
+  $('#nav #navbarSupportedContent > ul > li.nav-item').each((index, node) => {
+    $(node).removeClass('active');
+  });
+  // Seta a classe de ativo para o link da página atual
+  $(`#${paginaAtual}`).addClass('active');
+}
+
+function carregaProdutos(filtro) {
+  return new Promise((resolve, reject) => {
+    window.carregouProdutos = false;
+  
+    $.get('/src/database/produtos.json', data => {
+      console.log('data', data);
+  
+      window.carregouProdutos = true;
+
+      const produtos = filtro ? data.filter(filtro) : data;
+      return resolve(produtos);
+    });
+  });
+}
