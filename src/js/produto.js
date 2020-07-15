7095b0f54d9164e937579f163fcff1bb6c90ea76
Sergio Eduardo Flores Junior <sergio.flores.junior17@gmail.com>
2020-07-14 23:51:56 -0300

    fix detalhamento produto

--- src/js/produto.js
+++ src/js/produto.js
@@ -3,25 +3,16 @@ $(document).ready(async () => {
 	var url = new URL(url_string);
 	var id = url.searchParams.get("idProduto");
 
-	var produto = await carregaProdutos(produto => (produto.idProduto == id));
+	const [produto] = await carregaProdutos(produto => produto.idProduto == id);
 
 	let html = await getComponente('produto');
 
 	html = html.replace(/\${idProduto}/g, produto.idProduto);
 	html = html.replace(/\${nome}/g, produto.nome);
 	html = html.replace(/\${preco}/g, formataMoeda(produto.preco));
-	html = html.replace(/\${modelo}/g, formataMoeda(produto.modelo));
-	html = html.replace(/\${marca}/g, formataMoeda(produto.marca));
-	html = html.replace(/\${descricao}/g, formataMoeda(produto.descricao));
+	html = html.replace(/\${modelo}/g, produto.modelo);
+	html = html.replace(/\${marca}/g, produto.marca);
+	html = html.replace(/\${descricao}/g, produto.descricao);
 
 	$('#produto').append(html);
-	
 });
-
-function getComponente(nome) {
-  return new Promise((resolve, reject) => {
-    $.get(`/src/views/${nome}.html`)
-      .then(data => resolve(data))
-      .catch(err => reject(err));
-  });
-} 
